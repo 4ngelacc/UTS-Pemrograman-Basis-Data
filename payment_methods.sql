bf6ac3c0e1a4e9c8bb627e5006354995fbc3990b
renoain <rnoainn@gmail.com>
2025-10-20 01:04:17 +0700

    Update payment_methods.sql

--- payment_methods.sql
+++ payment_methods.sql
@@ -1,10 +1,11 @@
-INSERT INTO payment_methods (name, type) VALUES
-('Transfer BCA', 'bank'),
-('Transfer Mandiri', 'bank'),
-('Transfer BRI', 'bank'),
-('GoPay', 'ewallet'),
-('OVO', 'ewallet'),
-('DANA', 'ewallet'),
-('QRIS', 'qris'),
-('Tunai', 'cash');
-
+INSERT INTO payment_methods (id, name, type, is_active, created_at) VALUES
+(1, 'Bank Transfer - BCA', 'bank', true, CURRENT_TIMESTAMP),
+(2, 'Bank Transfer - Mandiri', 'bank', true, CURRENT_TIMESTAMP),
+(3, 'Bank Transfer - BNI', 'bank', true, CURRENT_TIMESTAMP),
+(4, 'GoPay', 'ewallet', true, CURRENT_TIMESTAMP),
+(5, 'OVO', 'ewallet', true, CURRENT_TIMESTAMP),
+(6, 'ShopeePay', 'ewallet', true, CURRENT_TIMESTAMP),
+(7, 'DANA', 'ewallet', true, CURRENT_TIMESTAMP),
+(8, 'Credit Card - Visa', 'card', true, CURRENT_TIMESTAMP),
+(9, 'Credit Card - MasterCard', 'card', true, CURRENT_TIMESTAMP),
+(10, 'Tunai', 'cash', true, CURRENT_TIMESTAMP);
